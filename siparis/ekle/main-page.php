5af9d215ed55099db9398210d603de57799c5df6
Rifai Kuçi <rfai4747@hotmail.com>
2021-12-25 14:18:01 +0300

    Update main-page.php

--- siparis/ekle/main-page.php
+++ siparis/ekle/main-page.php
@@ -2,7 +2,7 @@
 include "../../netting/baglan.php";
 
 
-$firmasql = "SELECT * FROM tblfirma where firmaTurId =3 ";
+$firmasql = "SELECT * FROM tblfirma  ";
 $firmalar = $db->query($firmasql);
 
 $profillerrsql = "SELECT * FROM tblprofil";
